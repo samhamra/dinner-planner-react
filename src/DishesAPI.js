aefbd61f61a91560d4cd80440804391e35d3024f
Filip Kis <filip.kis@gmail.com>
2018-02-28 10:45:54 +0100

    Improving error handling

--- src/DishesAPI.js
+++ src/DishesAPI.js
@@ -1,21 +1,31 @@
-const API_KEY = 'YOUR_API_KEY'
+const httpOptions = {
+    headers: { 'X-Mashape-Key': 'YOUR_API_KEY' }
+  };
 
 
 function getAllDishes() {
-    return fetch('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search', {
-        headers: {
-            'X-Mashape-Key': API_KEY
-        }
-    })
+    const url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search'
+    return fetch(url,httpOptions)
     .then(processResponse)
-    .catch(error => console.log('getAllDishes() API Error:', error.statusText, error))
+    .catch(handleError)
 }
 
 function processResponse (response) {
     if(response.ok){
         return response.json()
     }
-    throw response
+    throw response;
+}
+
+function handleError (error){
+    if(error.json) {
+        error.json().then( error =>{
+            console.error('getAllDishes() API Error:', error.message || error)    
+        })
+    } else {
+        console.error('getAllDishes() API Error:', error.message || error )    
+    }
+
 }
 
 export { getAllDishes }
