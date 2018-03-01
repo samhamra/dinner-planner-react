0687e1c0bd04a37bb0aebbd578c1d8a72738af15
Michel Tabari <tabari@kth.se>
2018-03-01 12:54:35 +0100

    Remove unnecessary constructor

--- src/SelectDish/SelectDish.js
+++ src/SelectDish/SelectDish.js
@@ -4,10 +4,6 @@ import Sidebar from '../Sidebar/Sidebar';
 import Dishes from '../Dishes/Dishes';
 
 class SelectDish extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
       <div className="SelectDish">
